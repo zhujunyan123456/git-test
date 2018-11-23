8ee9a879263d75f8eab1dc80fcce454fa97e4f63
zjy111111 <1341758041@qq.com>
2018-11-23 10:42:52 +0800

    工程添加数据

---
+++ gitProject/src/com/itheima/GitTest.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("1 2");
+        System.out.println("1 2");
+        System.out.println("1 2");
+        System.out.println("1 2");
+        System.out.println("1 3");
+        System.out.println("1 4");
+    }
+}
